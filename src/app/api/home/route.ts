11a857164b68d707cc0732963a9e7676b3d75ff3
Maisa Oliveira <maisaolisilva@gmail.com>
2024-11-24 13:57:42 -0300

    os dados dos animais da página home não são salvos mais em cache para que possa exibir os novos animais cadastrados

--- src/app/api/home/route.ts
+++ src/app/api/home/route.ts
@@ -9,8 +9,10 @@ export async function GET(request: Request) {
     try {
         const  animals = await Animal.find()
         return NextResponse.json(
-            animals
-            , { status: 200 })
+                animals,
+                { status: 200,
+                headers: {'Cache-Control': 'no-store'}
+                })
     } catch (error) {
         console.error('Erro ao buscar animais: ', error)
         return NextResponse.json({ message: 'Erro ao buscar animais.' }, { status: 500 })
