e058d356b44aeaf24d043e53c14ff2ce2c256464
Maisa Oliveira <maisaolisilva@gmail.com>
2024-11-28 00:14:45 -0300

    tentando concertar um erro que apareceu no deploy relacionada ao componete Rodape

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import styled from "styled-components";
 import Cabecalho from "../Cabecalho";
-import Rodape from "../Rodape";
+import Rodape from "@/components/Rodape";
 
 interface MainProps {
   children: React.ReactNode;
